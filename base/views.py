d8c683e86fe81fbc7d03cd6411119dd26d99765d
masasandeep <masasandeep12@gmail.com>
2024-04-27 19:22:38 +0530

    new branch

--- base/views.py
+++ base/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
-    return render(request,'base/main.html')
+    game = Games.objects.all()
+    context = {'game':game}
+    return render(request,'base/main.html',context)
+def gaming_profile(request,pk):
+    game_pf = BattleRoyale.objects.filter(Game__name=pk)
+    context = {'pf':game_pf}
+    return render(request,'base/profile.html',context)
