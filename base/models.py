c0bf5c472d6acd67f6586df9c6ec2c4e5611e346
masasandeep <masasandeep12@gmail.com>
2024-04-27 17:00:17 +0530

    ch

--- base/models.py
+++ base/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Freefire(models.Model):
-    pass
+    name = models.CharField( max_length=50)
+    cls = models.CharField( max_length=50)
+    def __str__(self):
+        return self.name
+    
