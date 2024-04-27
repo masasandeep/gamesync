593f00dccf1411665c6a88428053de1055d83146
masasandeep <masasandeep12@gmail.com>
2024-04-27 17:27:16 +0530

    models

--- base/models.py
+++ base/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Freefire(models.Model):
-    name = models.CharField( max_length=50)
-    cls = models.CharField( max_length=50)
+class BattleRoyale(models.Model):
+    name = models.CharField(max_length=150)
+    player_id = models.CharField(max_length=50,null=True,blank=True)
+    Rank = models.CharField(max_length=150)
+    kd_ratio = models.FloatField()
+    headshot_rate = models.FloatField()
+    no_of_headshots = models.IntegerField()
+    top_3_ratio = models.FloatField()
+    Avg_damage = models.FloatField()
     def __str__(self):
         return self.name
+
     
